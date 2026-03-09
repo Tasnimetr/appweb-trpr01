8bf0fd32449b5c0c852c694fa95ea8c35dcdebcf
Tasnime <tr.tasnime@gmail.com>
2026-03-08 21:43:07 -0400

    Modification logo

--- trpr01/my-app/src/App.vue
+++ trpr01/my-app/src/App.vue
@@ -12,7 +12,6 @@ import { editedFlower, duplicatedFlower } from './products';
 <template>
   <div class="container1">
     <img src="./assets/logo.jpg" alt="logo" />
-    <!--1 flower bouquet-->
     <h1>Fleuriste Ste-foy</h1>
   </div>
 
@@ -32,22 +31,6 @@ import { editedFlower, duplicatedFlower } from './products';
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-
-
 .container1 {
   display: flex;
   align-items: center;
